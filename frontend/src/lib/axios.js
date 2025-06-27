fd83089681246ae80ec6e3590812a06fb6575a7c
deepak-bca22 <deepak.bca22@gmail.com>
2025-06-27 12:39:28 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://haychatai.netlify.app/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://hay-chat-with-ai.vercel.app" : "/api",
   withCredentials: true,
 });
